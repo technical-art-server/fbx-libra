83a97c8b25d034e5c3f2141299915bc77cf4091f
Tack2 <takumi236@gmail.com>
2024-05-27 22:01:11 +0900

    create追加

--- src/FbxLibra/main.cpp
+++ src/FbxLibra/main.cpp
@@ -8,20 +8,29 @@ int main(int argc, char ** argv) {
         Command check(SL("check"), ValueOptions::ManyValues, false);
         check.setDescription(SL("Check the fbx."));
 
-        Arg arg('f', SL("fbx"), true, true);
-        check.addArg(arg);
+        Arg fbx('f', SL("fbx"), true, true);
+        check.addArg(&fbx);
+
+        Command create(SL("create"), ValueOptions::ManyValues, false);
+        create.addArg(&fbx);
 
         Help help;
         help.setAppDescription(SL("help"));
         help.setExecutable(argv[0]);
 
         cmd.addArg(&check);
+        cmd.addArg(&create);
         cmd.addArg(help);
 
         cmd.parse();
 
         if (cmd.isDefined("check")){
             for (const auto & value : cmd.values("check")){
+                outStream() << value << "\n";
+            }
+            outStream() << cmd.value("-f") << "\n";
+        } else if (cmd.isDefined("create")){
+            for (const auto & value : cmd.values("create")){
                 outStream() << value << "\n";
             }
             outStream() << cmd.value("-f") << "\n";
