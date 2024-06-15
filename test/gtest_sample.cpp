1311239d9f6c8313d86274ef765a8f00eca95715
yohawing <yohawing@gmail.com>
2024-06-15 19:28:43 +0900

    [update] TESTの環境でFbxLibraの依存を追加。
    ・現状は無理やりだけど、もっとコンパクトなCmakeにしたい。。。ライブラリ化しないとテスト難しいのか？

--- test/gtest_sample.cpp
+++ test/gtest_sample.cpp
@@ -1,4 +1,7 @@
 #include <gtest/gtest.h>
+#include <FbxLibra.h>
+#include "CounterWeight/HierarchyCounterWeight.h"
+#include "CounterWeight/HierarchyCounterWeightFactory.h"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -6,4 +9,20 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HierarchyCounterWeight, Check_Hierarchy) {
+	CounterWeightFactory* factory;
+	CounterWeight* weight;
+	CounterWeight* fbx_weight;
+	factory = new HierarchyCounterWeightFactory();
+	weight = factory->Load("dragon.hcw");
+	fbx_weight = factory->Create("dragon.fbx");
+	Status result = FBXLibraClient::Weigh(weight, fbx_weight);
+	EXPECT_EQ(result, Status::SUCCESS);
+
+	delete factory;
+	delete weight;
+	delete fbx_weight;
+
 }
