6602ae109cb608593cb98918f5e43947d48a4a09
Tack2 <takumi236@gmail.com>
2024-06-17 21:47:25 +0900

    失敗するテストケースの追加

--- test/gtest_sample.cpp
+++ test/gtest_sample.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+﻿#include <gtest/gtest.h>
 #include <FbxLibra.h>
 #include "CounterWeight/HierarchyCounterWeight.h"
 #include "CounterWeight/HierarchyCounterWeightFactory.h"
@@ -15,12 +15,11 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_EQ(7 * 6, 42);
 }
 
-TEST(HierarchyCounterWeight, Check_Hierarchy) {
+TEST(HierarchyCounterWeight, Check_Hierarchy_True) {
 	CounterWeightFactory* factory;
 	CounterWeight* weight;
 	CounterWeight* fbx_weight;
 	factory = new HierarchyCounterWeightFactory();
-
 	
 	// テスト用にCmakeからプロジェクトのパスを取得
 	cout << "Project path: " << PROJECT_PATH << endl;
@@ -39,5 +38,30 @@ TEST(HierarchyCounterWeight, Check_Hierarchy) {
 	delete factory;
 	delete weight;
 	delete fbx_weight;
+}
 
+TEST(HierarchyCounterWeight, Check_Hierarchy_False) {
+    CounterWeightFactory* factory;
+    CounterWeight* weight;
+    CounterWeight* fbx_weight;
+    factory = new HierarchyCounterWeightFactory();
+
+
+    // テスト用にCmakeからプロジェクトのパスを取得
+    cout << "Project path: " << PROJECT_PATH << endl;
+
+    auto fbxPath = string(PROJECT_PATH) + "/test/data/" + "fbx/stanford_bunny.fbx";
+    auto weightPath = string(PROJECT_PATH) + "/test/data/" + "cw/dragon.hcw";
+
+    cout << "FBX Path: " << fbxPath << endl;
+    cout << "Weight Path: " << weightPath << endl;
+
+    weight = factory->Load( weightPath );
+    fbx_weight = factory->Create(fbxPath);
+    Status result = FBXLibraClient::Weigh(weight, fbx_weight);
+    EXPECT_EQ(result, Status::FAILURE);
+
+    delete factory;
+    delete weight;
+    delete fbx_weight;
 }
