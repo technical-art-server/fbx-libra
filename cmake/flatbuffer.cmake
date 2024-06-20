562231de40b32dc80e6aa85280a312c8e03afb54
yohawing <yohawing@gmail.com>
2024-06-20 18:52:04 +0900

    [update] Flatc用のCMakeの更新。
    ConanのFlatcを使う。複数のSchemaファイルのビルド。テストの依存調整

--- cmake/flatbuffer.cmake
+++ cmake/flatbuffer.cmake
@@ -1,27 +1,28 @@
 find_package(flatbuffers)
 
-# ConanでインストールされたFlatBuffersのインストールディレクトリを指定
-set(FLATBUFFERS_INSTALL_DIR "path/to/flatbuffers")
-
-# flatcの実行ファイルを指定
-set(FLATC_EXECUTABLE "${FLATBUFFERS_INSTALL_DIR}/bin/flatc")
-
 # スキーマファイルのパスを複数指定
-set(SCHEMA_FILE "${CMAKE_SOURCE_DIR}/examples/data/hcw.fbs")
+set(SCHEMA_FILES
+    "${CMAKE_SOURCE_DIR}/examples/data/hcw.fbs"
+    "${CMAKE_SOURCE_DIR}/examples/data/vcw.fbs"
+)
 
 # 生成されるソースファイルの出力ディレクトリを指定
 set(GENERATED_DIR "${CMAKE_BINARY_DIR}/generated")
 
-# 生成されるヘッダーファイルとソースファイル
-set(GENERATED_FILES
-    "${GENERATED_DIR}/hcw_generated.h"
-)
+# 生成されるヘッダーファイルとソースファイルをリスト
+set(GENERATED_FILES)
+
+# スキーマファイルから生成されるファイルをリストに追加
+foreach(SCHEMA_FILE ${SCHEMA_FILES})
+    get_filename_component(SCHEMA_NAME ${SCHEMA_FILE} NAME_WE)
+    list(APPEND GENERATED_FILES "${GENERATED_DIR}/${SCHEMA_NAME}_generated.h")
+endforeach()
 
 # スキーマファイルからコードを生成するカスタムコマンドを追加
 add_custom_command(
     OUTPUT ${GENERATED_FILES}
-    COMMAND ${FLATC_EXECUTABLE} --cpp -o ${GENERATED_DIR} ${SCHEMA_FILE}
-    DEPENDS ${SCHEMA_FILE}
+    COMMAND ${FLATBUFFERS_FLATC_EXECUTABLE} --cpp -o ${GENERATED_DIR} ${SCHEMA_FILES}
+    DEPENDS ${SCHEMA_FILES}
     COMMENT "Running flatc compiler on ${SCHEMA_FILE}"
 )
 
