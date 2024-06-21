086853d05e35c62cd40696f1b2efecde8f58e21d
Tack2 <takumi236@gmail.com>
2024-06-21 23:06:37 +0900

    作成テストを追加

---
+++ test/gtest_create_hierarchy.cpp
@@ -0,0 +1,30 @@
+﻿#include "gtest/gtest.h"
+
+#include "CounterWeight/HierarchyCounterWeight.h"
+#include "CounterWeight/HierarchyCounterWeightFactory.h"
+#include <filesystem>
+#include "config.h"
+#include "FBXLibra.h"
+
+TEST(HierarchyCounterWeight, Create_Hierarchy) {
+    HierarchyCounterWeightFactory factory;
+
+    // テスト用にCmakeからプロジェクトのパスを取得
+    std::cout << "Project path: " << PROJECT_PATH << std::endl;
+
+    auto fbxPath = std::string(PROJECT_PATH) + "/test/data/" + "fbx/dragon.fbx";
+    auto outputPath = std::filesystem::temp_directory_path() / "dragon.hcw";
+
+    factory.Create(fbxPath);
+    CounterWeight::Save(*factory.GetBuilder(), outputPath);
+
+    EXPECT_EQ(true, std::filesystem::exists(outputPath));
+
+    // 実際に作って比較してみる
+    HierarchyCounterWeightFactory checkFactory;
+    auto weight = checkFactory.Load(outputPath);
+    auto fbx_weight = checkFactory.Create(fbxPath);
+    Status result = FBXLibraClient::Weigh(weight, fbx_weight);
+
+    EXPECT_EQ(result, Status::SUCCESS);
+}
