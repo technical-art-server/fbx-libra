01619039d0e641d2dc920d407bea523e5cfe128f
yohawing <yohawing@gmail.com>
2024-06-30 16:39:36 +0900

    fix fbx-sdk installation14 fix cmake

--- cmake/fbxsdk.cmake
+++ cmake/fbxsdk.cmake
@@ -6,13 +6,13 @@ if (FBX_SDK_ROOT)
 endif()
 
 # 環境変数に入ってるかチェック
-if (NOT EXISTS ${FBX_SDK_ROOT})
+if (NOT EXISTS "${FBX_SDK_ROOT}")
     message(STATUS "FBX SDK found at ENV ${FBX_SDK_ROOT}")
     set(FBX_SDK_ROOT $ENV{FBX_SDK_PATH} PATH "Path to root of fbx-sdk")
 endif()
 
 # 環境変数に入ってない場合はデフォルトのパスを設定
-if (NOT EXISTS ${FBX_SDK_ROOT})
+if (NOT EXISTS "${FBX_SDK_ROOT}")
     if(NOT DEFINED FBX_SDK_VERSION)
         set(FBX_SDK_VERSION 2020.3.7 STRING "fbx sdk version")
     endif()
@@ -36,12 +36,12 @@ if (NOT EXISTS ${FBX_SDK_ROOT})
 endif()
 
 # パスが存在しない場合はエラー
-if (NOT EXISTS ${FBX_SDK_ROOT})
+if (NOT EXISTS "${FBX_SDK_ROOT}")
     message(FATAL_ERROR "FBX SDK not found at ${FBX_SDK_ROOT}")
 else()
     set(FBX_SDK_INCLUDE_DIR ${FBX_SDK_ROOT}/include)
     set(FBX_SDK_LIB_DIR ${FBX_SDK_ROOT}/lib/x64)
-    if (NOT EXISTS ${FBX_SDK_LIB_DIR})
+    if (NOT EXISTS "${FBX_SDK_LIB_DIR}")
         set(FBX_SDK_LIB_DIR ${FBX_SDK_ROOT}/lib/vs2015/x64)
     endif()
     set(FBX_SDK_FOUND TRUE)
